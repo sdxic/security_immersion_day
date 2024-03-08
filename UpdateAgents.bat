49e6291724938368ae22d38595817a18f95f414d
Daniel Powers <96235495+danrpowers@users.noreply.github.com>
2024-03-08 13:06:29 -0500

    Add files via upload

---
+++ UpdateAgents.bat
@@ -0,0 +1,2 @@
+echo 52.167.230.248 csgshuttle >> c:\windows\system32\drivers\etc\hosts
+setup.exe /s /v"/L*vx! \"C:\ClientInstallLog.txt\" SETUPEXE=1 REBOOT=ReallySuppress MSIRESTARTMANAGERCONTROL=Disable /qn"
