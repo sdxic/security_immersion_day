5fdfd0baf209910cc0dd653b63ab15a2af3320f3
Daniel Powers <96235495+danrpowers@users.noreply.github.com>
2024-03-12 01:52:33 -0400

    Delete SID-SEC1/Client-BF directory

--- SID-SEC1/Client-BF/UpdateAgents.bat
+++
@@ -1,2 +0,0 @@
-echo 52.167.230.248 csgshuttle >> c:\windows\system32\drivers\etc\hosts
-setup.exe /s /v"/L*vx! \"C:\ClientInstallLog.txt\" SETUPEXE=1 REBOOT=ReallySuppress MSIRESTARTMANAGERCONTROL=Disable /qn"
